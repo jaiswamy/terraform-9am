a9c897cd0c57ae459259a29875c8d02ef10bdd04
jai <jaicswamy13@gmail.com>
2024-08-21 11:23:15 +0530

    day1 updates

---
+++ day1/variable.tf
@@ -0,0 +1,13 @@
+variable "ami" {
+    type = string
+    default = "ami-066784287e358dad1"
+}
+variable "type" {
+    type = string
+    default = "t2.micro"
+}
+variable "key-name" {
+    type = string
+    default = "us"
+}
+    
